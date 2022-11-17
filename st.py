d262e03412b18736af1fccf010971e2f1b286b48
haziqfaiz <1201302740@student.mmu.edu.my>
2022-11-17 19:24:29 +0800

    something

--- st.py
+++ st.py
@@ -1,12 +1,10 @@
 import folium
 import streamlit as st
-
+import pandas as pd
 from streamlit_folium import st_folium
 
-import folium
-
 # Make an empty map
-m = folium.Map(location=[20,0], tiles="OpenStreetMap", zoom_start=2)
+m = folium.Map(location=[3.1,101], tiles="OpenStreetMap", zoom_start=20)
 
 # Show the map
 
