e48f17bbafe46715a25ee09a57d973d7058feb8d
haziqfaiz-mmu <1201302740@student.mmu.edu.my>
2022-11-17 21:12:50 +0800

    added markers

--- st.py
+++ st.py
@@ -4,8 +4,15 @@ import pandas as pd
 from streamlit_folium import st_folium
 
 # Make an empty map
-m = folium.Map(location=[3.1,101], tiles="OpenStreetMap", zoom_start=20)
+m = folium.Map(location=[3.8,101.8], tiles="OpenStreetMap", zoom_start=10,prefer_canvas = True)
 
-# Show the map
+df = pd.read_csv("data.csv")
+# add marker one by one on the map
+for i in range(0,len(df)):
+    folium.Marker(
+        location=[df.iloc[i]['lat'], df.iloc[i]['lon']],
+        #popup=df.iloc[i]['name'],
+    ).add_to(m)
 
+# Show the map without markers
 st_data = st_folium(m, width=725)
